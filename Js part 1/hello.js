338fdfeb99a4219a36ba70cb078e29d9d45e5852
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-01 17:13:42 +0545

    first

--- Js part 1/hello.js
+++ Js part 1/hello.js
@@ -2,3 +2,5 @@ alert("Hey you HIIII");
 
 // this is the alert for the user's introduction
 alert("I am miss Rakshya");
+
+// statements are all the thing that you do javascript and ends with a semicolon
