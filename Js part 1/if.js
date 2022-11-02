959a445137e40e7b7af9683f7d3614979c35455c
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-02 16:40:39 +0545

    if statement

--- Js part 1/if.js
+++ Js part 1/if.js
@@ -1,10 +1,11 @@
-var youLikeMeat = true;
-var youDontLikeMeat = false;
+// var xBox = true;
+// var barbie = false;
 
-if (youDontLikeMeat) {
-  document.write("Here is the meat mean....");
-  console.log(youLikeMeat);
+var boy = "play station";
+var girl = "girl house";
+
+if (boy == "play station") {
+  document.write("let's play " + boy);
 } else {
-  document.write("Hey welcome to isha yoga center");
-  console.log(youDontLikeMeat);
+  document.write("let's play at my " + girl);
 }
