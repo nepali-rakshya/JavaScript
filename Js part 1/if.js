7f3eb1d60feb7eb9807f8591bfb1b7b2613cb6b6
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-02 14:32:47 +0545

    yes

---
+++ Js part 1/if.js
@@ -0,0 +1,10 @@
+var youLikeMeat = true;
+var youDontLikeMeat = false;
+
+if (youDontLikeMeat) {
+  document.write("Here is the meat mean....");
+  console.log(youLikeMeat);
+} else {
+  document.write("Hey welcome to isha yoga center");
+  console.log(youDontLikeMeat);
+}
