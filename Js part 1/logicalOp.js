4cecd38be348db95bed77b3f4cf3c609ee57f10f
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-03 14:22:54 +0545

    logical operators

---
+++ Js part 1/logicalOp.js
@@ -0,0 +1,12 @@
+var myAge = "26";
+
+if (/*myAge <= 18 || myAge >= 30 || */ myAge === 26 /*|| myAge <= 40*/) {
+  document.write("you are coming");
+} else {
+  document.write("you ain't coming");
+}
+
+var a = 10;
+
+a += 10;
+document.write(a);
