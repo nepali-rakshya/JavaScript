394b46ad1f7e908b5db8010c17422f5fb4b82b36
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-03 15:14:49 +0545

    for

---
+++ Js part 1/for.js
@@ -0,0 +1,18 @@
+var links = document.getElementsByTagName("a");
+
+for (i = 0; i < links.length; i++) {
+  console.log("This is link number " + (i + 1));
+}
+
+var para = document.getElementsByClassName("here");
+
+for (i = 0; i < para.length; i++) {
+  console.log("How many are there?... " + (i + 1));
+}
+
+var a = 2;
+
+while (a < 5) {
+  alert("i made your day! ;P");
+  a++;
+}
