95b57fd0d7ca2b36b4fa4d15ea2ef990c29d7b53
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-03 17:18:18 +0545

    practical

---
+++ Js part 1/practical.js
@@ -0,0 +1,12 @@
+var links = document.getElementsByTagName("a");
+
+// for (var i = 0; i < links.length; i++) {
+//   links[i].className = "links--" + i;
+// }
+
+var i = 0;
+
+while (i < links.length) {
+  links[i].className = "rakshya--" + i;
+  i++;
+}
