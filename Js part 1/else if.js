2a4d70c2f92a87321c83dbee3bfbc1472eeff023
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-03 13:46:02 +0545

    else if

--- Js part 1/else if.js
+++ Js part 1/else if.js
@@ -1,11 +1,9 @@
-var myAge = 15;
+var myAge = 19;
 
-if (myAge > 30) {
-  document.write("you are over 30!");
-} else if (myAge > 20) {
-  document.write("you are over 20!");
+if (myAge > 20) {
+  document.write("you are young");
 } else if (myAge > 10) {
-  document.write("you are over 10!");
+  document.write("you are old");
 } else {
-  document.write("you are not over 10!");
+  document.write("what is this?!");
 }
