6272843591267241d3305bfe6163693d999a1dc7
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-04 09:40:37 +0545

    global var

--- Js part 2/avg.js
+++ Js part 2/avg.js
@@ -12,3 +12,14 @@ console.log(avg); //2
 var sum = myAvg(2, 2); //2
 sum += sum; // sum = sum + sum = 2 + 2
 console.log(sum); // 4
+
+// *******************************************
+
+function mul(a, b) {
+  multiply = a * b;
+  console.log(multiply);
+}
+
+mul(2, 2);
+
+console.log("let's do the multiplication " + multiply * 10);
