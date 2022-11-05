32155bd305eace4a8d13515d53cb178d1a0b086d
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-05 19:37:16 +0545

    array

--- Js part 2/array.js
+++ Js part 2/array.js
@@ -1,6 +1,18 @@
 var house = ["1BHK", 1500, "2BHK", 2000, "3BHK", 3000];
 var len = house.length;
 
-for (var i = 0; i < len - 3; i++) {
-  alert("There is a house of " + house[i + i]);
-}
+// for (var i = 0; i < len - 3; i++) {
+//   alert("There is a house of " + house[i + i]);
+// }
+
+// var i = 0;
+// while (i < len - 3) {
+//   alert("There are flats of Rs. " + house[2 * i + 1]);
+//   i++;
+// }
+
+var names = 'Ronald\'s dad loves his "BOOK"';
+
+var nameChanged = names.slice(6, 9);
+
+console.log(nameChanged);
