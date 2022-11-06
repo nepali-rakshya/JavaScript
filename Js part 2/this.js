775b6aa12314b4ee31d6ac27cdd75e1a5a402f71
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-06 15:14:45 +0545

    this

--- Js part 2/this.js
+++ Js part 2/this.js
@@ -7,7 +7,34 @@ var myCar = {
   drive: function (speed, time) {
     console.log(speed * time);
   },
+  run: function () {
+    console.log(this.driver + " likes running.");
+  },
+  details: function () {
+    console.log(this);
+  },
 };
 
-console.log(myCar.maxSpeed, myCar.walk(), myCar.drive(50, 3));
+console.log(myCar.details());
+console.log(myCar.run());
+// console.log(myCar.maxSpeed, myCar.walk(), myCar.drive(50, 3));
 // console.log(myCar.drive(50, 3));
+
+var myBarbie = {
+  names: "Barbie",
+  eyes: "beautiful",
+  color: "fair",
+  hair: "silky",
+  dance: function () {
+    console.log("Her dance is " + this.eyes);
+  },
+  laugh: function () {
+    console.log("Her name is " + this.names);
+  },
+  details: function () {
+    console.log(this);
+  },
+};
+
+console.log(myBarbie.details());
+console.log(myBarbie.dance());
