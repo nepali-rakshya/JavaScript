9398372399dcab6cb81ee7eee5e03da3994c3131
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-06 06:23:40 +0545

    object

---
+++ Js part 2/object.js
@@ -0,0 +1,10 @@
+// The way to create an instance of an object is:
+
+var myString = new String();
+myString = "hello world";
+console.log(myString.length);
+
+// The short hand version to create an instance of an object is:
+
+var myString2 = "hello there nice to meet you";
+console.log(myString2.length);
