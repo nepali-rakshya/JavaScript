282e75e6cdd6627d91d91ab6c1440461971b70e1
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-07 13:03:48 +0545

    date

--- Js part 2/date.js
+++ Js part 2/date.js
@@ -1,2 +1,35 @@
-var myDate = new Date();
+var myDate = new Date(2000, 10, 29, 9, 15, 00);
 console.log(myDate);
+
+console.log("year " + myDate.getFullYear());
+console.log("month " + myDate.getMonth());
+console.log("date " + myDate.getDate());
+console.log("hours " + myDate.getHours());
+console.log("minutes " + myDate.getMinutes());
+
+console.log("miliseconds " + myDate.getMilliseconds());
+
+console.log("miliseconds " + myDate.getTime());
+
+var myDate1 = new Date(2000, 10, 29, 9, 15, 00);
+console.log(myDate1.getTime());
+
+if (myDate.getTime() === myDate1.getTime()) {
+  console.log("equal");
+} else {
+  console.log("not equal");
+}
+
+var one = {
+  names: "Rakshya",
+};
+
+var two = {
+  names: "Rakshya",
+};
+
+if (one === two) {
+  console.log("equal");
+} else {
+  console.log("not equal");
+}
