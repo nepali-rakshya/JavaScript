5ee9018107bd6cde87206953b136ab8c011e21da
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-07 15:23:58 +0545

    dom

---
+++ Js part 3/dom.js
@@ -0,0 +1,6 @@
+var myDiv = document.getElementsByClassName("container");
+console.log(myDiv);
+console.log(myDiv.getElementsByClassName("row"));
+
+var myArray = [1, 2, 3, 4, 5];
+console.log(myArray[1]);
