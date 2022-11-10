44471e5d647838b6cbc035420ca448573d767ec1
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-10 13:45:24 +0545

    DOM

--- Js part 3/dom2.js
+++ Js part 3/dom2.js
@@ -1,3 +1,23 @@
-var title = document.querySelector("#main-heading");
-title.style.backgroundColor = "red";
-console.log(title);
+// *********** STYLING ELEMENTS *************
+
+// var title = document.querySelector("#main-heading");
+// title.style.backgroundColor = "red";
+// console.log(title);
+
+// *********** CREATING ELEMENTS *************
+
+var newHead = document.createElement("h1");
+var oldHead = document.querySelector("h1");
+
+oldHead.append(newHead);
+
+newHead.innerText = "And Stars";
+
+newHead.style.fontSize = "3rem";
+
+// ************** MODIFYING ATT. AND CLASSES **************
+
+newHead.setAttribute("class", "main-heading");
+
+var list = document.querySelector("li");
+console.log(list.getAttribute("class"));
