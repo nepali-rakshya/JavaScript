0793eeb23f21a70c7d5f613146db7ee7a456f6f1
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-10 12:26:14 +0545

    dom1

---
+++ Js part 3/dom1.js
@@ -0,0 +1,11 @@
+// var title = document.getElementById("main-heading");
+// console.log(title);
+
+// var group = document.getElementsByClassName("container");
+// console.log(group);
+
+// var tag = document.getElementsByTagName("li");
+// console.log(tag);
+
+// var queryOnly = document.querySelectorAll(".list-items");
+// console.log(queryOnly);
