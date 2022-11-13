09f2c95186b61ee8cef04984818c47af8177cb92
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-13 11:59:17 +0545

    listener

--- Js part 3/listener.js
+++ Js part 3/listener.js
@@ -1,4 +1,16 @@
-// D O M 
+// D O M
 // R E V E A L    E V E N T
 
+const revealBtn = document.querySelector("button");
 
+const hiddenContent = document.querySelector(".hidden-content");
+
+revealBtn.addEventListener("click", magicButton);
+
+function magicButton() {
+  if (hiddenContent.classList.contains("reveal-btn")) {
+    hiddenContent.classList.remove("reveal-btn");
+  } else {
+    hiddenContent.classList.add("reveal-btn");
+  }
+}
