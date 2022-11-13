ef2c1e488958ab33e0e0047ea20645f448631ef0
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-13 12:49:00 +0545

    event listener

--- Js part 3/listener.js
+++ Js part 3/listener.js
@@ -14,3 +14,20 @@ function magicButton() {
     hiddenContent.classList.add("reveal-btn");
   }
 }
+
+const imgHover = document.querySelector(".img-adjust");
+
+const realImage = document.querySelector("img");
+
+function magicImage() {
+  if (realImage.classList.contains("hidden-content")) {
+    realImage.classList.remove("hidden-content");
+    realImage.classList.add("reveal-btn");
+    realImage.setAttribute("id", "distance");
+  } else {
+    realImage.classList.remove("reveal-btn");
+    realImage.classList.add("hidden-content");
+  }
+}
+
+imgHover.addEventListener("mouseover", magicImage);
