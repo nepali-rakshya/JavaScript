20690e60a25fe5ff812c86174aa46acace9e618d
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-14 20:46:50 +0545

    for

--- Js part 4/for.js
+++ Js part 4/for.js
@@ -4,14 +4,25 @@
 //   hobbies: "Coding",
 // };
 
-const obj = [3, 4, 5, 6];
+// const obj = [3, 4, 5, 6];
 
 // console.log(Object.keys(obj).length);
 
 // Using Traditional For Loop
 
-for (let i = 0; i < Object.keys(obj).length; i++) {
-  console.log(Object.keys(obj)[i]);
-}
+// for (let i = 0; i < Object.keys(obj).length; i++) {
+//   console.log(Object.keys(obj)[i]);
+// }
 
 // FOR ............ IN LOOP
+
+var num = 0;
+var num = 10;
+
+let num2 = 55;
+let num1 = 34;
+let num3;
+
+const num4 = 88;
+
+console.log(num4);
