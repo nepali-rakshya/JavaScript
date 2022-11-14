560fe0cb154b0cf41945094d604dde834e60f92d
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-14 16:17:55 +0545

    commit

---
+++ Js part 4/for.js
@@ -0,0 +1,17 @@
+// const obj = {
+//   name: "Rakshya",
+//   language: "JavaScript",
+//   hobbies: "Coding",
+// };
+
+const obj = [3, 4, 5, 6];
+
+// console.log(Object.keys(obj).length);
+
+// Using Traditional For Loop
+
+for (let i = 0; i < Object.keys(obj).length; i++) {
+  console.log(Object.keys(obj)[i]);
+}
+
+// FOR ............ IN LOOP
