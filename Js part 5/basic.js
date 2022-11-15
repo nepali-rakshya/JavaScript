b858b5361783902861fba4b1eb49fabc23fd7d78
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-15 13:26:22 +0545

    basic

---
+++ Js part 5/basic.js
@@ -0,0 +1,14 @@
+let b = 10;
+b = 20;
+
+const obj = {
+  rakshya: "good",
+  nepali: true,
+  pair: 2,
+};
+
+// console.log(obj);
+
+for (let i = 0; i < Object.keys(obj).length; i++) {
+  console.log(obj[Object.keys(obj)[i]]);
+}
