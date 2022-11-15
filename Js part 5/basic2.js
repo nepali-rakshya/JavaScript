777415e7d486224234419acad5fb7af39f1c7bfc
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-15 15:16:22 +0545

    basic

--- Js part 5/basic2.js
+++ Js part 5/basic2.js
@@ -1,3 +1,21 @@
 let a = prompt("Hi, what's your name?");
 console.log(parseInt(a));
 console.log(a.toString());
+
+const luck = prompt("Choose your luck");
+switch (luck) {
+  case "1":
+    console.log("Try again.");
+    break;
+  case "2":
+    console.log("Try again..");
+    break;
+  case "3":
+    console.log("Try again...");
+    break;
+  case "rakshya":
+    console.log("You Won!!!");
+    break;
+  default:
+    console.log("You have no luck today");
+}
