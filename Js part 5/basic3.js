083e53cc960ebbde752395ce87ef20ec52f9ea43
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-16 12:00:51 +0545

    best

--- Js part 5/basic3.js
+++ Js part 5/basic3.js
@@ -1,68 +1,100 @@
-// let value = parseInt(prompt("Enter a range"));
-// let sum = 0;
-// for (let i = 0; i < value; i++) {
-//   sum += i + 1;
+// // let value = parseInt(prompt("Enter a range"));
+// // let sum = 0;
+// // for (let i = 0; i < value; i++) {
+// //   sum += i + 1;
+// // }
+
+// // console.log(sum);
+
+// // const marks = {
+// //   rakshya: 100,
+// //   aadarsha: 99,
+// //   navin: 98,
+// //   sita: 77,
+// //   chiranjibi: 40,
+// // };
+
+// // for (m in marks) {
+// //   console.log(m + " " + marks[m]);
+// // }
+
+// // let arr = ["hehe", "hello", "rakshya", 6000];
+
+// // for (mrBeast of arr) {
+// //   console.log(mrBeast);
+// // }
+
+// // for (mrBeast in arr) {
+// //   console.log(mrBeast);
+// //   console.log(arr[mrBeast]);
+// // }
+
+// // let i = 1;
+// // do {
+// //   console.log(i);
+// //   i++;
+// // } while (i == 0);
+
+// function sum(ram, b, c, d) {
+//   let num1 = (ram + b) / 2;
+//   let num2 = (b + c) / 2;
+//   let num3 = (c + d) / 2;
+
+//   console.log(Math.round(num1) + " " + num2 + " " + num3);
 // }
 
-// console.log(sum);
+// sum(1, 2, 3, 4);
 
-// const marks = {
-//   rakshya: 100,
-//   aadarsha: 99,
-//   navin: 98,
-//   sita: 77,
-//   chiranjibi: 40,
+// const mul = (a, b) => {
+//   console.log(a * b);
+// };
+// mul(3, 8);
+
+// const sub = (a, b) => {
+//   console.log(a - b);
 // };
 
-// for (m in marks) {
-//   console.log(m + " " + marks[m]);
+// sub(10, 5);
+
+// const obj = {
+//   // names: "rakshya",
+//   hobby: () => {
+//     console.log("I love coding");
+//   },
+// };
+
+// for (key in obj) {
+//   obj[key]();
 // }
 
-// let arr = ["hehe", "hello", "rakshya", 6000];
+// const obj = {
+//   harry: 98,
+//   rohan: 45,
+//   rakshya: 99,
+// };
 
-// for (mrBeast of arr) {
-//   console.log(mrBeast);
+// for (let i = 0; i < Object.keys(obj).length; i++) {
+//   let display = Object.keys(obj)[i];
+//   console.log(Object.keys(obj)[i] + " " + obj[display]);
+// }
+// console.log("");
+// console.log("**********Using for in loop************");
+// console.log("");
+
+// for (const key in obj) {
+//   console.log(key + " " + obj[key]);
 // }
 
-// for (mrBeast in arr) {
-//   console.log(mrBeast);
-//   console.log(arr[mrBeast]);
-// }
-
-// let i = 1;
-// do {
-//   console.log(i);
-//   i++;
-// } while (i == 0);
-
-function sum(ram, b, c, d) {
-  let num1 = (ram + b) / 2;
-  let num2 = (b + c) / 2;
-  let num3 = (c + d) / 2;
-
-  console.log(Math.round(num1) + " " + num2 + " " + num3);
-}
-
-sum(1, 2, 3, 4);
-
-const mul = (a, b) => {
-  console.log(a * b);
-};
-mul(3, 8);
-
-const sub = (a, b) => {
-  console.log(a - b);
-};
-
-sub(10, 5);
-
-const obj = {
-  // names: "rakshya",
-  hobby: () => {
-    console.log("I love coding");
-  },
-};
-
-for (key in obj) {
-  obj[key]();
+let i = 0;
+while (i >= 0) {
+  let guess = parseInt(prompt("Guess the lukcy num"));
+  let luckyNum = 10;
+  if (guess === luckyNum) {
+    console.log("You are one lucky kid");
+    break;
+  } else {
+    console.log("Try again");
+    // continue;
+  }
+  i++;
 }
