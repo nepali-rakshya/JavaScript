0551769e94b1c84f5d2a05fc2f7b6ae951f4015c
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-18 13:11:12 +0545

    basic 5

---
+++ Js part 5/basic5.js
@@ -0,0 +1,37 @@
+// for each method in array
+
+let num = [1, 2, 3, 4, 5, 6];
+// let looped = num.forEach(arrayNumber);
+
+// function arrayNumber(number) {
+//   console.log(number);
+// }
+
+// num.forEach((number, index) => {
+//   console.log(number + " " + index);
+// });
+
+// // Array from
+
+// let name = "122324345";
+
+// console.log(Array.from(name));
+
+// *******************************************
+// for of and for in loop
+
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+// }
+
+// for (key in num) {
+//   console.log(num[key]);
+// }
+
+// for (const key of num) {
+//   console.log(key);
+// }
+
+// *************************************************
+
+// MAP    FILTER    REDUCE
