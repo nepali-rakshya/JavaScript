8a039a2d3480419a545126474674d797986fd5fa
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-22 17:44:45 +0545

    concept

--- Js part 6/example.js
+++ Js part 6/example.js
@@ -3,3 +3,29 @@ for (let row of mytab1.rows) {
     let val = (cell.innerText = "help"); // your code below
   }
 }
+
+let array = ["rakshya", "is", "a", "good", "girl"];
+
+let space = "";
+for (let i = array.length; i > 0; i--) {
+  space += i + " ";
+  console.log(space);
+}
+// console.log(space);
+
+// for (key in array) {
+//   console.log(typeof key);
+// }
+
+// for (let key of array) {
+//   console.log(key);
+// }
+
+// let obj = {
+//   name: "rakshya",
+//   roll: 5,
+// };
+
+// for (key in obj) {
+//   console.log(obj[key]);
+// }
