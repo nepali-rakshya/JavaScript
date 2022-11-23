0ce310db0b82fd2f8b0f8d226d9c30b2bc58f3ac
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-23 13:14:52 +0545

    basic table

--- Js part 6/basic3.js
+++ Js part 6/basic3.js
@@ -37,17 +37,17 @@
 
 // console.log(tableCheck);
 
-let thirdRow = document.querySelector("table");
+// let thirdRow = document.querySelector("table");
 
-let lastRow = thirdRow.lastElementChild.lastElementChild.cells;
-let value = Array.from(lastRow);
-console.log(value);
+// let lastRow = thirdRow.lastElementChild.lastElementChild.cells;
+// let value = Array.from(lastRow);
+// console.log(value);
 
-console.log(Array.isArray(lastRow));
+// console.log(Array.isArray(lastRow));
 
-console.log([...lastRow]);
+// console.log([...lastRow]);
 
-console.log(Array.isArray(...lastRow));
+// console.log(Array.isArray(...lastRow));
 
 // [...lastRow].forEach((element) => {
 //   element.innerText = "help";
@@ -59,3 +59,30 @@ console.log(Array.isArray(...lastRow));
 //   console.log(val);
 // }
 // }
+
+document.querySelector(".heading").style.color = "red";
+
+let heading = document.querySelector("table").rows[0];
+
+for (let element of heading.cells) {
+  console.log((element.style.color = "blue"));
+}
+
+let content = document.querySelector("table").lastElementChild;
+// console.log(content);
+
+for (let row of content.rows) {
+  for (let col of row.cells) {
+    console.log((col.style.color = "green"));
+  }
+}
+
+for (let row of content.rows) {
+  for (let col of row.cells) {
+    if (isNaN(col.innerText) === false) {
+      console.log((col.style.color = "white"));
+    }
+  }
+}
+
+let foot = (document.querySelector(".below").style.color = "purple");
