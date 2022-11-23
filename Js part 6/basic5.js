eb80ef27142878d538646eca6f0c20735c33dd6e
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-23 14:10:32 +0545

    basics

---
+++ Js part 6/basic5.js
@@ -0,0 +1,7 @@
+// ***************************************************
+// P R A C T I C E    Q U E S T I O N
+
+let navBar = document.querySelector("ul").firstElementChild.firstElementChild;
+console.log((navBar.style.color = "red"));
+
+console.log(navBar);
