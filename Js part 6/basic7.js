cb6b00643109392060a75485e2546e4ae3116fb0
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-24 22:23:46 +0545

    basic

--- Js part 6/basic7.js
+++ Js part 6/basic7.js
@@ -1,9 +1,17 @@
 // appendChild append prepend before after replaceWith
 
-let value = document.createElement("p");
-value.innerHTML = "<strong>I AM ATOMIC </strong>";
-let newLine = document.body.firstElementChild.nextElementSibling;
-// console.log(newLine.appendChild(value));
-// console.log(newLine.append("HOLA"));
-// console.log(newLine.appendChild(value));
-// console.log(newLine.replaceWith(value));
+// let value = document.createElement("p");
+// value.innerHTML = "<strong>I AM ATOMIC </strong>";
+// let newLine = document.body.firstElementChild.nextElementSibling;
+// // console.log(newLine.appendChild(value));
+// // console.log(newLine.append("HOLA"));
+// // console.log(newLine.appendChild(value));
+// // console.log(newLine.replaceWith(value));
+// newLine.remove();
+
+let newLine = document.body.firstElementChild.nextElementSibling.children;
+// console.log(newLine[1].classList.add("blue", "red", "purple"));
+
+// console.log((newLine[2].className = "red"));
+
+console.log(newLine[2].classList.toggle("red"));
