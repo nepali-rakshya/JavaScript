d5d15c887d632c68f04b97e10257146474d44e8a
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-24 08:20:07 +0545

    basic

--- Js part 6/basic5.js
+++ Js part 6/basic5.js
@@ -1,11 +1,11 @@
 // ***************************************************
 // P R A C T I C E    Q U E S T I O N
 
-let navBar = document.querySelector("ul").firstElementChild.firstElementChild;
-console.log((navBar.style.color = "red"));
+// let navBar = document.querySelector("ul").firstElementChild.firstElementChild;
+// console.log((navBar.style.color = "red"));
 
-console.log(navBar);
+// console.log(navBar);
 
-Array.from(document.querySelectorAll("li")).forEach((element) => {
-  element.style.background = "cyan";
-});
+// Array.from(document.querySelectorAll("li")).forEach((element) => {
+//   element.style.background = "cyan";
+// });
