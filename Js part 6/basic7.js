28393a84a1c85191a6219dfdfab68a4270d7eda7
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-24 20:37:07 +0545

    basic

---
+++ Js part 6/basic7.js
@@ -0,0 +1,9 @@
+// appendChild append prepend before after replaceWith
+
+let value = document.createElement("p");
+value.innerHTML = "<strong>I AM ATOMIC </strong>";
+let newLine = document.body.firstElementChild.nextElementSibling;
+// console.log(newLine.appendChild(value));
+// console.log(newLine.append("HOLA"));
+// console.log(newLine.appendChild(value));
+// console.log(newLine.replaceWith(value));
