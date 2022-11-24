50c50f038784069d06b169f0f4df63de24d5ab15
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-24 20:12:33 +0545

    basic

--- Js part 6/basic6.js
+++ Js part 6/basic6.js
@@ -2,4 +2,17 @@
 
 // document.body.firstElementChild.firstElementChild.firstElementChild.outerHTML;
 
-document.body.firstElementChild.firstElementChild.firstElementChild.hidden = false;
+// let val = document.body.firstElementChild.firstElementChild.firstElementChild;
+
+// val.setAttribute("hidden", "true");
+// console.log(val.getAttribute("hidden"));
+
+// console.log(val.parentElement.setAttribute("id", "specific"));
+
+// console.log(val.parentElement.getAttribute("id"));
+
+// console.log(val.parentElement.removeAttribute("id"));
+
+// console.log(val.parentElement.attributes.length);
+
+// console.log(val.parentElement.hasAttribute("id"));
