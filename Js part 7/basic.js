d79696e2b59ddcbbe025cd624532200f80e6d956
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-25 18:08:00 +0545

    basic

--- Js part 7/basic.js
+++ Js part 7/basic.js
@@ -1,16 +1,33 @@
 let backG = document.body;
 
-let firstTime = setTimeout(() => {
-  console.log(backG.classList.toggle("pink"));
-}, 1000);
+// let firstTime = setTimeout(() => {
+//   console.log(backG.classList.toggle("pink"));
+// }, 1000);
 
-let secondTime = setTimeout(() => {
-  backG.style.background = "red";
-}, 2000);
+// let secondTime = setTimeout(() => {
+//   backG.style.background = "red";
+// }, 2000);
 
-// let count = 1;
-// let thirdSame = setInterval(function () {
-//   alert("count " + count);
-//   backG.style.background = "skyblue";
-//   count++;
-// }, 5000);
+console.time("first");
+let element = ["black", "pink", "grey", "skyblue", "red", "blue", "green"];
+
+let count = 0;
+
+let thirdSame;
+
+for (let i = 0; i <= element.length; i++) {
+  thirdSame = setInterval(function () {
+    alert("Color " + element[i]);
+    backG.style.background = element[i];
+  }, 100);
+}
+
+clearInterval(thirdSame);
+
+console.timeEnd("first");
+// let a = "you are ";
+// let func = (b) => {
+//   console.log(a + b);
+// };
+
+// func("hola");
