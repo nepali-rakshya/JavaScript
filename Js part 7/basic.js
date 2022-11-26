41807f20208035a1911c21bbd8c5ae4811d6b879
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-26 18:22:52 +0545

    basic

--- Js part 7/basic.js
+++ Js part 7/basic.js
@@ -10,11 +10,18 @@ let backG = document.body;
 
 let element = ["black", "pink", "grey", "skyblue", "red", "blue", "green"];
 let count = 0;
-let value = setInterval(() => {
-  alert(`Color ${element[count]}`);
-  backG.style.background = element[count];
-  if (count === element.length - 1) {
-    clearInterval(value);
-  }
-  count++;
-}, 1000);
+// let value = setInterval(() => {
+//   alert(`Color ${element[count]}`);
+//   backG.style.background = element[count];
+//   if (count === element.length - 1) {
+//     clearInterval(value);
+//   }
+//   count++;
+// }, 1000);
+
+for (let i = 0; i < element.length; i++) {
+  setTimeout(() => {
+    alert(`Color ${element[i]}`);
+    backG.style.background = element[i];
+  }, 1000);
+}
