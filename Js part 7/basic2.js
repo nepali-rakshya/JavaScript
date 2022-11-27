a17de248e3c2e1731e13f0bd2912070364876586
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-27 17:59:29 +0545

    basics

--- Js part 7/basic2.js
+++ Js part 7/basic2.js
@@ -1,7 +1,27 @@
 let b = document.getElementsByTagName("button")[0];
-b.onclick = () => {
-  alert("Hola my fellas");
+// b.onclick = () => {
+//   alert("Hola my fellas");
+// };
+// b.onclick = () => {
+//   alert("We are good");
+// };
+
+let x = () => {
+  console.log("Hey gorgeous");
 };
-b.onclick = () => {
-  alert("We are good");
+
+b.addEventListener("click", x);
+
+// let a = prompt("Tell me a joke");
+
+// if (a === "yes") {
+//   b.removeEventListener("click", x);
+// }
+let mouse = (e) => {
+  console.log(e.type);
+  console.log(e.target);
+  console.log(e.clientX, e.clientY);
+  console.log("Mickey Mouse");
 };
+
+b.addEventListener("mouseover", mouse);
