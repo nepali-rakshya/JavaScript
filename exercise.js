8fac9b0d6b73fa7df0721e6d5aa5192b4931db45
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-28 17:06:48 +0545

    exercise

---
+++ exercise.js
@@ -0,0 +1,38 @@
+let firstButton = document.body.firstElementChild;
+
+let one = () => {
+  let color = (firstButton.style.background = "pink");
+  alert(`Now you will get a ${color} color`);
+};
+
+firstButton.addEventListener("click", one);
+
+let secondButton = document.body.firstElementChild.nextElementSibling;
+
+secondButton.addEventListener("click", function two() {
+  let color = (secondButton.style.color = "red");
+  alert(`The color is ${color}`);
+});
+
+let thirdButton = document.getElementById("third");
+
+thirdButton.addEventListener("click", function three() {
+  let padding = (thirdButton.style.padding = "1.3em");
+  alert(`The padding is ${padding}`);
+});
+
+let values = ["aadarsha", "rakshya", "sita", "chiranjibi", "sujal"];
+
+// let values = [1111, 1, 11111, 12323, 1144545];
+
+// function compare(a, b) {
+//   return a - b;
+// }
+
+// console.log(
+//   values.sort((a, b) => {
+//     return b - a;
+//   })
+// );
+
+console.log(values.sort());
