c2e5541c3c8e8733ec8764f2df9f6bbd207c59cf
nepali-rakshya <rakshyanepali440@gmail.com>
2022-11-30 16:14:38 +0545

    exercise completed

--- Js part 7/exercise3.js
+++ Js part 7/exercise3.js
@@ -1,10 +1,26 @@
 let link = document.getElementById("youtube");
 
-link.addEventListener("click", function () {
-  let newD = document.createElement("div");
-  link.parentNode.insertBefore(newD, link);
-  newD.append(link);
+// link.addEventListener("click", function () {
+//   // let newD = document.createElement("div");
+//   // link.parentNode.insertBefore(newD, link);
+//   // newD.append(link);
+//   // link.before(newD);
+//   // newD.append(link);
 
-  link.before(newD);
-  newD.append(link);
+//   window.location = "https://www.google.com/";
+// });
+
+// const fetchContent = async (url) => {
+//   con = await fetch(url);
+//   let a = await con.json();
+//   return a;
+// };
+
+// setInterval(async function () {
+//   let url = "https://jsonplaceholder.typicode.com/todos/1";
+//   console.log(await fetchContent(url));
+// }, 5000);
+
+link.addEventListener("click", () => {
+  link.classList.toggle("bulb");
 });
