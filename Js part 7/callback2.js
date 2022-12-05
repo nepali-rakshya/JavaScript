bf628f4208e8e8d143c637ddfaaaaf965782d831
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-05 14:34:57 +0545

    callback

---
+++ Js part 7/callback2.js
@@ -0,0 +1,16 @@
+// function sayCode(checkCallback) {
+//   let code = prompt("What is the code");
+//   let error = new Error("Access denied");
+//   checkCallback(code, error);
+// }
+
+// function checkCode(code, error) {
+//   if (code === "rak123") {
+//     console.warn("Access granted");
+//     return;
+//   } else {
+//     console.error(error);
+//   }
+// }
+
+// sayCode(checkCode);
