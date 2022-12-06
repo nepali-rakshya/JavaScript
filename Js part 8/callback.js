35d70da6786b9bdfcc00aa61ea76a5e2598f2344
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-06 13:43:01 +0545

    callback

---
+++ Js part 8/callback.js
@@ -0,0 +1,23 @@
+// function display(sum) {
+//   console.log("The sum is " + sum);
+// }
+
+// function sum(a, b) {
+//   let sum = a + b;
+//   console.log(sum);
+//   return sum;
+// }
+
+// let value = sum(1, 2);
+// display(value);
+
+// function sum(a, b, callbackDisplay) {
+//   let sum = a + b;
+//   callbackDisplay(sum);
+// }
+
+// sum(1, 2, function display(sum) {
+//   console.log("The sum is " + sum);
+// });
+
+const myCalcul
