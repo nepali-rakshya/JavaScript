e18cafca5d9efb85fc935fc8a01ac204807847a2
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-06 14:57:15 +0545

    callback

--- Js part 8/callback.js
+++ Js part 8/callback.js
@@ -20,15 +20,52 @@
 //   console.log("The sum is " + sum);
 // });
 
-const myCalc = (
-  a,
-  b,
-  display = function (sum) {
-    console.log("The sum is " + sum);
-  }
-) => {
-  let sum = a + b;
-  display(sum);
-};
+// const myCalc = (
+//   a,
+//   b,
+//   display = function (sum) {
+//     console.log("The sum is " + sum);
+//   }
+// ) => {
+//   let sum = a + b;
+//   display(sum);
+// };
 
-myCalc(1, 2);
+// myCalc(1, 2);
+
+// function display() {
+//   console.log("hehe");
+//   return "poo";
+// }
+
+// const display1 = () => display();
+
+// const display2 = function () {
+//   return display1();
+// };
+
+// console.log(display2());
+
+let rakPlay = new Promise((resolve, reject) => {
+  let name = prompt("What's your name?");
+  resolve((() => name)());
+});
+
+rakPlay // its a parameter not function
+  .then((result) => {
+    // const display = (result) => {
+    setTimeout(() => console.log("love you " + result), 2000);
+    // };
+    // display;
+  });
+
+//   .then((result2) => result2 + " love")
+//   .then((result3) => {
+//     // let value = prompt("Anything else?");
+
+//     console.log(result3 + " you ");
+//   });
+
+// setTimeout(() => {
+//   console.log("love you");
+// }, 1000);
