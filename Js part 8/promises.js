9dd81501dead2dbafdf8e52d3d79aa770fb17ace
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-07 17:35:34 +0545

    callback

---
+++ Js part 8/promises.js
@@ -0,0 +1,66 @@
+// let promise1 = new Promise((resolve, reject) => {
+//   for (let i = 0; i < 5; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//       resolve("done");
+//     }, 5000);
+//   }
+// });
+
+// let promise2 = new Promise((resolve, reject) => {
+//   for (let i = 0; i < 5; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+//   resolve("done2");
+// });
+
+// console.log(promise1);
+
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, i * 1000);
+// }
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, i * 1000);
+// }
+
+// setTimeout(() => {
+//   console.log("hola");
+//   console.log("I am done");
+// }, 1000);
+// setTimeout(() => {
+//   console.log("bye");
+//   console.log("I am not done");
+// }, 1000);
+
+// let promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("hey promise 1");
+//     resolve("done");
+//   }, 1000);
+// });
+// let promise2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("hey promise 2");
+//     reject(new Error("Its not valid"));
+//   }, 1000);
+// });
+
+// promise2.catch((value) => {
+//   console.log(value);
+// });
+
+// console.log(promise1, promise2);
+
+let p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(true);
+  });
+});
+
+p.then(() => alert("hola"));
