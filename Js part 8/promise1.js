ea5985a293574b6f2f85150dfb9616e62dd309e7
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-08 12:07:54 +0545

    promise

--- Js part 8/promise1.js
+++ Js part 8/promise1.js
@@ -1,24 +1,91 @@
-let promise1 = new Promise((res, rej) => {
+// let promise1 = new Promise((res, rej) => {
+//   setTimeout(() => {
+//     console.log("It has been resolved");
+//     res("rakshya");
+//   }, 1000);
+//   //   rej(new Error("There's an issue"));
+// });
+
+// promise1
+//   .then((name) => {
+//     console.log(name);
+//     return new Promise((resolve, reject) => {
+//       resolve("There is no escape");
+//     });
+//   })
+//   .then((escape) => {
+//     console.log(escape);
+//     return new Promise((resolve, reject) => {
+//       reject(new Error("Here is a problem"));
+//     });
+//   })
+//   .catch(function p(problem) {
+//     console.log(problem);
+//   });
+
+/********************************************************/
+
+//// CAT   MEME   CODE
+
+// let first = false;
+// let second = false;
+
+// function display() {
+//   if (first) {
+//     return "I am first";
+//   } else if (second) {
+//     return "I am second";
+//   } else {
+//     return "I failed";
+//   }
+// }
+
+// console.log("Rakshya " + display());
+
+// function name(
+//   display = (name) => {
+//     console.log(name);
+//   }
+// ) {
+//   let n = prompt("Name: ");
+//   display(n);
+// }
+
+// name();
+
+// let promise = new Promise((resolve, reject) => {
+//   let n = prompt("Name:");
+//   resolve(n);
+// });
+
+// promise.then((name) => {
+//   console.log(name);
+// });
+
+let promise1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log("It has been resolved");
-    res("rakshya");
-  }, 1000);
-  //   rej(new Error("There's an issue"));
+    alert("Hello I am resolved");
+    // resolve("Done");
+  }, 2000);
+  reject("Sorry");
 });
 
 promise1
-  .then((name) => {
-    console.log(name);
-    return new Promise((resolve, reject) => {
-      resolve("There is no escape");
-    });
-  })
-  .then((escape) => {
-    console.log(escape);
-    return new Promise((resolve, reject) => {
-      reject(new Error("Here is a problem"));
-    });
-  })
-  .catch((problem) => {
-    console.log(problem);
+  .then(
+    (resolve) => {
+      console.log(resolve + " yes!");
+    },
+    (reject) => {
+      console.log(reject + " no");
+      // return reject;
+      return new Promise((resolve, reject) => {
+        if (true) {
+          console.log("We are done");
+          resolve("yes!");
+        }
+      });
+    }
+  )
+  .then((value) => {
+    console.log("Please try again " + value);
   });
