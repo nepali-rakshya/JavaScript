f63fe6ea67e9a1b1ad594f2831130e360a972f6c
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-08 13:05:55 +0545

    promise game

---
+++ Js part 8/promiseGame.js
@@ -0,0 +1,92 @@
+// let userInput = () => {
+//   let user = prompt("Rock, Paper, Scissor:");
+//   let userInitial = user.charAt(0).toUpperCase();
+//   return userInitial;
+// };
+
+// let botInput = () => {
+//   let valueArr = ["Rock", "Paper", "Scissor"];
+//   let botVal = valueArr[Math.floor(Math.random() * valueArr.length)];
+//   let botInitial = botVal.charAt(0);
+//   return botInitial;
+// };
+
+// let check = (callbackUser, callbackBot) => {
+//   let user = callbackUser();
+//   console.log(user);
+//   let bot = callbackBot();
+//   console.log(bot);
+
+//   if (
+//     (user === "R" && bot === "R") ||
+//     (user === "P" && bot === "P") ||
+//     (user === "S" && bot === "S")
+//   ) {
+//     console.log("It's a tie");
+//   } else if (
+//     (user === "R" && bot === "P") ||
+//     (user === "P" && bot === "S") ||
+//     (user === "S" && bot === "R")
+//   ) {
+//     console.log("The bot wins");
+//   } else if (
+//     (user === "R" && bot === "S") ||
+//     (user === "P" && bot === "R") ||
+//     (user === "S" && bot === "P")
+//   ) {
+//     console.log("The user wins");
+//   }
+// };
+
+// check(userInput, botInput);
+
+//
+//
+// ***********************************************************
+//
+//
+//  P R O M I S E S
+
+let botInput = () => {
+  let valueArr = ["Rock", "Paper", "Scissor"];
+  let botVal = valueArr[Math.floor(Math.random() * valueArr.length)];
+  let botInitial = botVal.charAt(0);
+  return botInitial;
+};
+
+let promise1 = new Promise((resolve, reject) => {
+  let value = prompt("Rock, Paper, Scissor:");
+  let userInitial = value.charAt(0).toUpperCase();
+  resolve(userInitial);
+});
+
+promise1.then((userVal, botInput) => {
+  let valueArr = ["Rock", "Paper", "Scissor"];
+  let botVal = valueArr[Math.floor(Math.random() * valueArr.length)];
+  let botInitial = botVal.charAt(0);
+  let user = userVal;
+  console.log("User: " + user);
+  let bot = botInitial;
+  console.log("Bot: " + bot);
+  setTimeout(() => {
+    if (
+      (user === "R" && bot === "R") ||
+      (user === "P" && bot === "P") ||
+      (user === "S" && bot === "S")
+    ) {
+      console.log("It's a tie");
+    } else if (
+      (user === "R" && bot === "P") ||
+      (user === "P" && bot === "S") ||
+      (user === "S" && bot === "R")
+    ) {
+      console.log("The bot wins");
+    } else if (
+      (user === "R" && bot === "S") ||
+      (user === "P" && bot === "R") ||
+      (user === "S" && bot === "P")
+    ) {
+      console.log("The user wins");
+    }
+  }, 3000);
+});
