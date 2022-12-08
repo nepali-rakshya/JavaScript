29aa4e678a51149f99ce4ed8d4337b1b80bf7615
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-08 13:22:42 +0545

    promise game

--- Js part 8/promiseGame.js
+++ Js part 8/promiseGame.js
@@ -47,46 +47,48 @@
 //
 //  P R O M I S E S
 
-let botInput = () => {
-  let valueArr = ["Rock", "Paper", "Scissor"];
-  let botVal = valueArr[Math.floor(Math.random() * valueArr.length)];
-  let botInitial = botVal.charAt(0);
-  return botInitial;
-};
-
 let promise1 = new Promise((resolve, reject) => {
   let value = prompt("Rock, Paper, Scissor:");
   let userInitial = value.charAt(0).toUpperCase();
-  resolve(userInitial);
+  // resolve(userInitial);
+  reject(userInitial);
 });
 
-promise1.then((userVal, botInput) => {
-  let valueArr = ["Rock", "Paper", "Scissor"];
-  let botVal = valueArr[Math.floor(Math.random() * valueArr.length)];
-  let botInitial = botVal.charAt(0);
-  let user = userVal;
-  console.log("User: " + user);
-  let bot = botInitial;
-  console.log("Bot: " + bot);
-  setTimeout(() => {
-    if (
-      (user === "R" && bot === "R") ||
-      (user === "P" && bot === "P") ||
-      (user === "S" && bot === "S")
-    ) {
-      console.log("It's a tie");
-    } else if (
-      (user === "R" && bot === "P") ||
-      (user === "P" && bot === "S") ||
-      (user === "S" && bot === "R")
-    ) {
-      console.log("The bot wins");
-    } else if (
-      (user === "R" && bot === "S") ||
-      (user === "P" && bot === "R") ||
-      (user === "S" && bot === "P")
-    ) {
-      console.log("The user wins");
-    }
-  }, 3000);
-});
+promise1.then(
+  (userVal) => {
+    let valueArr = ["Rock", "Paper", "Scissor"];
+    let botVal = valueArr[Math.floor(Math.random() * valueArr.length)];
+    let botInitial = botVal.charAt(0);
+    let user = userVal;
+    console.log("User: " + user);
+    let bot = botInitial;
+    console.log("Bot: " + bot);
+    setTimeout(() => {
+      if (
+        (user === "R" && bot === "R") ||
+        (user === "P" && bot === "P") ||
+        (user === "S" && bot === "S")
+      ) {
+        console.log("It's a tie");
+      } else if (
+        (user === "R" && bot === "P") ||
+        (user === "P" && bot === "S") ||
+        (user === "S" && bot === "R")
+      ) {
+        console.log("The bot wins");
+      } else if (
+        (user === "R" && bot === "S") ||
+        (user === "P" && bot === "R") ||
+        (user === "S" && bot === "P")
+      ) {
+        console.log("The user wins");
+      }
+    }, 3000);
+  },
+  (res) => {
+    console.log(new Error(`This is invalid "${res}"`));
+  }
+);
+// .catch((res) => {
+//   console.log(new Error("This is invalid " + res));
+// });
