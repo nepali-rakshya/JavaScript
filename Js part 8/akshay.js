7d319c3a5634d858f2a5639e7495c3bdd96e4589
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-09 19:35:33 +0545

    confusion

--- Js part 8/akshay.js
+++ Js part 8/akshay.js
@@ -1,5 +1,38 @@
-const cart = ["shoes", "pants", "kurta"];
+// function storage(
+//   value = () => {
+//     setTimeout(() => {
+//       console.log("i am not gonna stop");
+//     }, 2000);
+//   }
+// ) {
+//   value();
+// }
 
-api.createOrder();
+// storage();
 
-api.proceedToPayment();
+// function welcome(greetingMsg, callback) {
+//   console.log("Before calling callback");
+//   setTimeout(() => {
+//     callback(greetingMsg);
+//   }, 0);
+//   console.log("After calling callback");
+// }
+// function sayMyName(greet) {
+//   console.log(greet + " Professor");
+// }
+// welcome("Hello", sayMyName);
+
+function getAPIRequest(callbackOnSuccess) {
+  var getReq = new XMLHttpRequest();
+  getReq.open("GET", "https://reqres.in/api/users?page=2");
+  getReq.send();
+  getReq.onload = () => {
+    if (getReq.status == 200) {
+      callbackOnSuccess(getReq);
+    }
+  };
+}
+function processSucessResponse(request) {
+  alert("The response is " + request.response);
+}
+getAPIRequest(processSucessResponse);
