7f665451500987bb126a1875e2bf94ac4a3aad0b
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-11 17:38:26 +0545

    basic

---
+++ Js part 8/akshay3.js
@@ -0,0 +1,15 @@
+// // getName();
+// // console.log(getName);
+
+// var x = 9;
+// // console.log(x);
+// // function getName() {
+// //   console.log("Hello");
+// // }
+
+// console.log("hello");
+// const a = 9;
+
+// let a = 10;
+// console.log(a);
+// // console.log(x);
