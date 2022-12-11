ad95e938a01a3a8596a82546e57cab024ffcedf7
nepali-rakshya <rakshyanepali440@gmail.com>
2022-12-11 18:31:45 +0545

    basic revision

--- Js part 8/akshay3.js
+++ Js part 8/akshay3.js
@@ -13,3 +13,26 @@
 // let a = 10;
 // console.log(a);
 // // console.log(x);
+
+// function animal() {
+//   if (true) {
+//     var animal1 = "cat";
+//     const animal2 = "dog";
+//     let animal3 = "rat";
+//   }
+//   console.log(animal1);
+//   console, log(animal2); //animal2 is not defiend
+//   console, log(animal3); //animal3 is not defiend
+// }
+// animal();
+
+function value(callbackValue) {
+  console.log("I am a value");
+  callbackValue();
+}
+
+value2 = () => {
+  console.log("You are a value");
+};
+
+value(value2);
